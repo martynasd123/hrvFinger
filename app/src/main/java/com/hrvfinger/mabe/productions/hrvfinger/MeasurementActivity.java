adb1064c9c4d8d94281f6802f0417e4c935e12f7
martynux112 <dargis.martynas@gmail.com>
2018-03-02 21:15:10 +0200

    initial commit

---
+++ app/src/main/java/com/hrvfinger/mabe/productions/hrvfinger/MeasurementActivity.java
@@ -0,0 +1,13 @@
+package com.hrvfinger.mabe.productions.hrvfinger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeasurementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurement);
+    }
+}
