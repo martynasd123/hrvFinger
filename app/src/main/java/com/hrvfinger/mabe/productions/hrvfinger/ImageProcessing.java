29b328d90b8be86e794d40bd27e4112a84bbffee
B <B@Valentinass-MacBook-Pro.local>
2018-03-03 14:02:29 +0200

    camera

--- app/src/main/java/com/hrvfinger/mabe/productions/hrvfinger/ImageProcessing.java
+++ app/src/main/java/com/hrvfinger/mabe/productions/hrvfinger/ImageProcessing.java
@@ -35,8 +35,7 @@ public abstract class ImageProcessing {
 
                 int pixel = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
                 int red = (pixel >> 16) & 0xff;
-                int green = (pixel >> 8) & 0xff;
-                sum += green; // I hear green works better than RED!
+                sum += red;
             }
         }
         return sum;
--- app/src/main/java/com/hrvfinger/mabe/productions/hrvfinger/MeasurementActivity.java
+++ app/src/main/java/com/hrvfinger/mabe/productions/hrvfinger/MeasurementActivity.java
@@ -1,12 +1,17 @@
 package com.hrvfinger.mabe.productions.hrvfinger;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 
 import android.hardware.Camera;
 import android.os.Build;
+
+import android.os.CountDownTimer;
 import android.os.PowerManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,22 +20,30 @@ import android.view.SurfaceView;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.apache.commons.lang3.ArrayUtils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
-public class MeasurementActivity extends AppCompatActivity {
+public class MeasurementActivity extends Activity {
 
-    public static int bpm;
+    private static final String TAG = "HeartRateMonitor";
+    private static final AtomicBoolean processing = new AtomicBoolean(false);
+
     private static SurfaceView preview = null;
     private static SurfaceHolder previewHolder = null;
     private static Camera camera = null;
+    private static String beatsPerMinuteValue="";
 
 
     private static int averageIndex = 0;
-    private static final int averageArraySize = 100;
+    private static final int averageArraySize = 4;
     private static final int[] averageArray = new int[averageArraySize];
-    private static final AtomicBoolean processing = new AtomicBoolean(false);
+    private static String strSavedDoctorID="";
+    private static Context parentReference = null;
+
     public static enum TYPE {
         GREEN, RED
     };
@@ -42,47 +55,39 @@ public class MeasurementActivity extends AppCompatActivity {
     }
 
     private static int beatsIndex = 0;
-    private static final int beatsArraySize = 14;
+    private static final int beatsArraySize = 3;
     private static final int[] beatsArray = new int[beatsArraySize];
-    private static final long[] timesArray = new long[beatsArraySize];
     private static double beats = 0;
     private static long startTime = 0;
 
-
-    static int counter = 0;
-
-    private static final int sampleSize = 256;
-    private static final CircularFifoQueue sampleQueue = new CircularFifoQueue(
-            sampleSize);
-    private static final CircularFifoQueue timeQueue = new CircularFifoQueue(
-            sampleSize);
-
-    public static final CircularFifoQueue bpmQueue = new CircularFifoQueue(40);
-
-    private static final FFT fft = new FFT(sampleSize);
-
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement);
-
         preview = (SurfaceView) findViewById(R.id.camera_preview);
         previewHolder = preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[] {Manifest.permission.CAMERA}, 1);
-            }
-        }
 
-
-
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+        startTime = System.currentTimeMillis();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onPause() {
         super.onPause();
@@ -92,32 +97,111 @@ public class MeasurementActivity extends AppCompatActivity {
         camera.stopPreview();
         camera.release();
         camera = null;
-        // try {
-        // out.close();
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-
-        bpm = -1;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+
+        int totalBPM = 0;
+        int totalCount = 0;
+
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onPreviewFrame(byte[] data, Camera cam) {
+            if (data == null) throw new NullPointerException();
+            Camera.Size size = cam.getParameters().getPreviewSize();
+            if (size == null) throw new NullPointerException();
+
+            if (!processing.compareAndSet(false, true)) return;
+
+            int width = size.width;
+            int height = size.height;
+
+            int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
+            // Log.i(TAG, "imgAvg="+imgAvg);
+            if (imgAvg == 0 || imgAvg == 255) {
+                processing.set(false);
+                return;
+            }
+
+            int averageArrayAvg = 0;
+            int averageArrayCnt = 0;
+            for (int i = 0; i < averageArray.length; i++) {
+                if (averageArray[i] > 0) {
+                    averageArrayAvg += averageArray[i];
+                    averageArrayCnt++;
+                }
+            }
+
+            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+            TYPE newType = currentType;
+            if (imgAvg < rollingAverage) {
+                newType = TYPE.RED;
+                if (newType != currentType) {
+                    beats++;
+                    // Log.d(TAG, "BEAT!! beats="+beats);
+                }
+            } else if (imgAvg > rollingAverage) {
+                newType = TYPE.GREEN;
+            }
+
+            if (averageIndex == averageArraySize) averageIndex = 0;
+            averageArray[averageIndex] = imgAvg;
+            averageIndex++;
+
+            // Transitioned from one state to another to the same
+            if (newType != currentType) {
+                currentType = newType;
+
+            }
+
+            long endTime = System.currentTimeMillis();
+            double totalTimeInSecs = (endTime - startTime) / 1000d;
+            if (totalTimeInSecs >= 1) {
+                double bps = (beats / totalTimeInSecs);
+                int dpm = (int) (bps * 60d);
+                if (dpm < 30 || dpm > 180) {
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                    processing.set(false);
+                    return;
+                }
+
+                // Log.d(TAG,
+                // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
+
+                if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                beatsArray[beatsIndex] = dpm;
+                beatsIndex++;
+
+                int beatsArrayAvg = 0;
+                int beatsArrayCnt = 0;
+                for (int i = 0; i < beatsArray.length; i++) {
+                    if (beatsArray[i] > 0) {
+                        beatsArrayAvg += beatsArray[i];
+                        beatsArrayCnt++;
+                    }
+                }
+                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+                totalBPM +=beatsAvg;
+                totalCount++;
+                if(totalCount==20){
+                    Log.i("TEST","TOtaaaaaaaaal" + String.valueOf(totalBPM));
+                }
+                Log.i("TEST", String.valueOf(beatsAvg));
+                beatsPerMinuteValue=String.valueOf(beatsAvg);
+
+                startTime = System.currentTimeMillis();
+                beats = 0;
+            }
+            processing.set(false);
+        }
+    };
 
 
 
-        camera = Camera.open();
-
-        // File file = new File(this.getFilesDir(), "data.txt");
-        // try {
-        // out = new BufferedWriter(new FileWriter(file));
-        // } catch (IOException e) {
-        // Log.e(TAG,"unexpected Error", e);
-        // e.printStackTrace();
-        // }
-
-    }
 
     private static SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
 
@@ -126,13 +210,11 @@ public class MeasurementActivity extends AppCompatActivity {
          */
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
-            Log.i("TEST","surfaceCreated");
             try {
                 camera.setPreviewDisplay(previewHolder);
                 camera.setPreviewCallback(previewCallback);
             } catch (Throwable t) {
-                Log.e("TEST",
-                        "Exception in setPreviewDisplay()", t);
+                Log.e("TEST", "Exception in setPreviewDisplay()", t);
             }
         }
 
@@ -140,15 +222,13 @@ public class MeasurementActivity extends AppCompatActivity {
          * {@inheritDoc}
          */
         @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                                   int height) {
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             Camera.Parameters parameters = camera.getParameters();
             parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
             Camera.Size size = getSmallestPreviewSize(width, height, parameters);
             if (size != null) {
                 parameters.setPreviewSize(size.width, size.height);
-                Log.d("TEST", "Using width=" + size.width + " height="
-                        + size.height);
+                Log.d(TAG, "Using width=" + size.width + " height=" + size.height);
             }
             camera.setParameters(parameters);
             camera.startPreview();
@@ -159,104 +239,41 @@ public class MeasurementActivity extends AppCompatActivity {
          */
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
-            camera.release();
             // Ignore
         }
     };
 
+    private static Camera.Size getSmallestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
 
-    private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
 
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void onPreviewFrame(byte[] data, Camera cam) {
-            if (data == null)
-                throw new NullPointerException();
-            Camera.Size size = cam.getParameters().getPreviewSize();
-            if (size == null)
-                throw new NullPointerException();
-
-            if (!processing.compareAndSet(false, true))
-                return;
-
-            int width = size.width;
-            int height = size.height;
-
-            int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(),
-                    height, width);
-
-            // try {
-            // out.write(imgAvg + ",");
-            // } catch (IOException e) {
-            // e.printStackTrace();
-            // }
-
-            sampleQueue.add((double) imgAvg);
-            timeQueue.add(System.currentTimeMillis());
-
-            double[] y = new double[sampleSize];
-            double[] x = ArrayUtils.toPrimitive((Double[]) sampleQueue
-                    .toArray(new Double[0]));
-            long[] time = ArrayUtils.toPrimitive((Long[]) timeQueue
-                    .toArray(new Long[0]));
-
-            if (timeQueue.size() < sampleSize) {
-                processing.set(false);
-
-                return;
-            }
-
-            double Fs = ((double) timeQueue.size())
-                    / (double) (time[timeQueue.size() - 1] - time[0]) * 1000;
-
-            fft.fft(x, y);
-
-            int low = Math.round((float) (sampleSize * 40 / 60 / Fs));
-            int high = Math.round((float) (sampleSize * 160 / 60 / Fs));
-
-            int bestI = 0;
-            double bestV = 0;
-            for (int i = low; i < high; i++) {
-                double value = Math.sqrt(x[i] * x[i] + y[i] * y[i]);
-
-                if (value > bestV) {
-                    bestV = value;
-                    bestI = i;
+                    if (newArea < resultArea) result = size;
                 }
             }
-
-            bpm = Math.round((float) (bestI * Fs * 60 / sampleSize));
-            Log.i("TEST", String.valueOf(bpm));
-            bpmQueue.add(bpm);
-
-
         }
 
-        };
-
-
-        private static Camera.Size getSmallestPreviewSize(int width, int height,
-                                                          Camera.Parameters parameters) {
-            Camera.Size result = null;
-
-            for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-                if (size.width <= width && size.height <= height) {
-                    if (result == null) {
-                        result = size;
-                    } else {
-                        int resultArea = result.width * result.height;
-                        int newArea = size.width * size.height;
-
-                        if (newArea < resultArea)
-                            result = size;
-                    }
-                }
-            }
-
-            return result;
-        }
-
-
+        return result;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
